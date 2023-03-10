414ae6d87397925c612bbd50b15d48338e3fe937
Vlad <vladbolkunovv@gmail.com>
2023-03-11 00:48:43 +0300

    feat: maps table

--- client/src/components/routes/maps/MapsListComponent.vue
+++ client/src/components/routes/maps/MapsListComponent.vue
@@ -1,5 +1,35 @@
-<template></template>
+<template>
+  <div class="container-lg mt-3">
+    <h3>Загруженные карты</h3>
+    <AgGridVue
+      class="ag-theme-alpine"
+      :column-defs="columnDefs"
+      :row-data="data"
+      :grid-options="options"
+    />
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { AgGridVue } from "ag-grid-vue3";
+import { ColDef, GridOptions } from "ag-grid-community";
+import { getDefaultGridOptions } from "@/ag-grid/factory";
+import { getMapsInfo } from "@/components/routes/maps/api";
+import { MapInfo } from "@/types/maps";
+
+const columnDefs: ColDef<MapInfo>[] = [
+  { headerName: "Id", field: "id", flex: 2 },
+  { headerName: "Дата загрузки", field: "date", flex: 5 },
+  { headerName: "Размер", field: "size", flex: 6 },
+  { headerName: "Обработано", field: "ready", flex: 7 },
+];
+
+const options: GridOptions<MapInfo> = {
+  ...getDefaultGridOptions(),
+  domLayout: "autoHeight",
+};
+
+const data = await getMapsInfo();
+</script>
 
 <style scoped lang="scss"></style>
