419a1669d80a84a37a354bce0e6336a0a7d279ce
Vlad <vladbolkunovv@gmail.com>
2023-03-11 00:00:26 +0300

    feat: ag-grid styles import

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,5 +3,7 @@ import { router } from "@/router";
 import App from "@/App.vue";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 createApp(App).use(router).mount("#app");
