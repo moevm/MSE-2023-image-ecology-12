aa06e8e95a86deb2411e64bf23e59274fad7a31d
Vlad <vladbolkunovv@gmail.com>
2023-03-11 00:59:00 +0300

    feat: queue table

--- client/src/components/routes/queue/QueueComponent.vue
+++ client/src/components/routes/queue/QueueComponent.vue
@@ -1,21 +1,37 @@
 <template>
-  <section class="container-md">
-    <ul class="list-group mt-1">
-      <QueueItem
-        v-for="image of queue"
-        :key="image.id + image.id"
-        :item="image"
-        class="list-group-item"
-      />
-    </ul>
-  </section>
+  <div class="container-lg mt-3">
+    <h3>Очередь обработки</h3>
+    <AgGridVue
+      class="ag-theme-alpine"
+      :column-defs="columnDefs"
+      :row-data="data"
+      :grid-options="options"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getQueue } from "@/components/routes/queue/api";
-import QueueItem from "@/components/routes/queue/components/QueueItem.vue";
+import { AgGridVue } from "ag-grid-vue3";
+import { ColDef, GridOptions } from "ag-grid-community";
+import { getDefaultGridOptions } from "@/ag-grid/factory";
+import { MapInfo } from "@/types/maps";
+import { getQueueInfo } from "@/components/routes/queue/api";
 
-const queue = await getQueue();
+const columnDefs: ColDef<MapInfo>[] = [
+  { headerName: "Id", field: "id", flex: 2, rowDrag: true },
+  { headerName: "Дата загрузки", field: "uploadDate", flex: 5 },
+  { headerName: "Прогресс", field: "progress", flex: 6 },
+  { headerName: "Статус", field: "status", flex: 7 },
+];
+
+const options: GridOptions<MapInfo> = {
+  ...getDefaultGridOptions(),
+  domLayout: "autoHeight",
+  rowDragManaged: true,
+  animateRows: true,
+};
+
+const data = await getQueueInfo();
 </script>
 
 <style scoped lang="scss"></style>
