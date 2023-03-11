54dfeca2f31e152155570c0f4ca11fed561c25de
Vlad <vladbolkunovv@gmail.com>
2023-03-11 16:06:03 +0300

    feat: reports actions

--- client/src/components/routes/reports/ReportsListComponent.vue
+++ client/src/components/routes/reports/ReportsListComponent.vue
@@ -6,6 +6,7 @@
       :column-defs="columnDefs"
       :row-data="data"
       :grid-options="options"
+      @grid-ready="fitActionsColumn"
     />
   </div>
 </template>
@@ -13,10 +14,18 @@
 <script setup lang="ts">
 import { AgGridVue } from "ag-grid-vue3";
 import { ColDef, GridOptions } from "ag-grid-community";
-import { getDefaultGridOptions } from "@/ag-grid/factory";
+import {
+  fitActionsColumn,
+  getActionsColDef,
+  getDefaultGridOptions,
+} from "@/ag-grid/factory";
 import { getReportsInfo } from "@/components/routes/reports/api";
 import { ReportInfo } from "@/types/reports";
 import { dateFormatter } from "@/ag-grid/formatters";
+import { routeNames } from "@/router";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const columnDefs: ColDef<ReportInfo>[] = [
   { headerName: "Id", field: "id", flex: 2 },
@@ -25,6 +34,24 @@ const columnDefs: ColDef<ReportInfo>[] = [
     field: "date",
     flex: 5,
     valueFormatter: dateFormatter,
+  },
+  {
+    ...getActionsColDef([
+      {
+        tooltip: "Открыть карту",
+        icon: "bi bi-map",
+        button: "btn-secondary",
+        onClicked: (action, data) =>
+          router.push({ name: routeNames.Map, params: { id: data.id } }),
+      },
+      {
+        tooltip: "Открыть отчёт",
+        icon: "bi bi-file-text",
+        button: "btn-primary",
+        onClicked: (action, data) =>
+          router.push({ name: routeNames.Map, params: { id: data.id } }),
+      },
+    ]),
   },
 ];
 
