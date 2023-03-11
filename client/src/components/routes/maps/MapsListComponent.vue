9f3e6a2794991123ee2ac27f67fe5377a45fc56f
Vlad <vladbolkunovv@gmail.com>
2023-03-11 15:10:50 +0300

    fix: actions column factory using

--- client/src/components/routes/maps/MapsListComponent.vue
+++ client/src/components/routes/maps/MapsListComponent.vue
@@ -13,12 +13,10 @@
 <script setup lang="ts">
 import { AgGridVue } from "ag-grid-vue3";
 import { ColDef, GridOptions } from "ag-grid-community";
-import { getDefaultGridOptions } from "@/ag-grid/factory";
+import { getActionsColDef, getDefaultGridOptions } from "@/ag-grid/factory";
 import { getMapsInfo } from "@/components/routes/maps/api";
 import { MapInfo } from "@/types/maps";
 import { dateFormatter } from "@/ag-grid/formatters";
-import ActionsRenderer from "@/components/renderers/ActionsRenderer.vue";
-import { Action } from "@/types/ag-grid/actions";
 import { useRouter } from "vue-router";
 import { routeNames } from "@/router";
 
@@ -35,31 +33,27 @@ const columnDefs: ColDef<MapInfo>[] = [
   { headerName: "Размер", field: "size", flex: 6 },
   { headerName: "Обработано", field: "ready", flex: 7 },
   {
-    headerName: "Действия",
-    cellRenderer: ActionsRenderer,
-    cellRendererParams: {
-      actions: [
-        {
-          tooltip: "Открыть карту",
-          icon: "bi bi-map",
-          button: "btn-primary",
-          onClicked: (action, data) =>
-            router.push({ name: routeNames.Map, params: { id: data.id } }),
-        },
-        {
-          tooltip: "Открыть отчёт",
-          icon: "bi bi-file-text",
-          button: "btn-primary",
-          onClicked: (action, data) =>
-            router.push({
-              /* name: reports */
-              name: routeNames.Map,
-              params: { id: data.id },
-            }),
-          hide: (data) => !data.ready,
-        },
-      ] as Action<MapInfo>[],
-    },
+    ...getActionsColDef([
+      {
+        tooltip: "Открыть карту",
+        icon: "bi bi-map",
+        button: "btn-primary",
+        onClicked: (action, data) =>
+          router.push({ name: routeNames.Map, params: { id: data.id } }),
+      },
+      {
+        tooltip: "Открыть отчёт",
+        icon: "bi bi-file-text",
+        button: "btn-primary",
+        onClicked: (action, data) =>
+          router.push({
+            /* name: reports */
+            name: routeNames.Map,
+            params: { id: data.id },
+          }),
+        hide: (data) => !data.ready,
+      },
+    ]),
   },
 ];
 
