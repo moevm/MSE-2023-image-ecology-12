9925efba37fc55541fb34ebf3e335b65208df92c
Vlad <vladbolkunovv@gmail.com>
2023-03-11 14:55:19 +0300

    feat: maps list actions

--- client/src/components/routes/maps/MapsListComponent.vue
+++ client/src/components/routes/maps/MapsListComponent.vue
@@ -17,6 +17,12 @@ import { getDefaultGridOptions } from "@/ag-grid/factory";
 import { getMapsInfo } from "@/components/routes/maps/api";
 import { MapInfo } from "@/types/maps";
 import { dateFormatter } from "@/ag-grid/formatters";
+import ActionsRenderer from "@/components/renderers/ActionsRenderer.vue";
+import { Action } from "@/types/ag-grid/actions";
+import { useRouter } from "vue-router";
+import { routeNames } from "@/router";
+
+const router = useRouter();
 
 const columnDefs: ColDef<MapInfo>[] = [
   { headerName: "Id", field: "id", flex: 2 },
@@ -28,6 +34,29 @@ const columnDefs: ColDef<MapInfo>[] = [
   },
   { headerName: "Размер", field: "size", flex: 6 },
   { headerName: "Обработано", field: "ready", flex: 7 },
+  {
+    headerName: "Действия",
+    cellRenderer: ActionsRenderer,
+    cellRendererParams: {
+      actions: [
+        {
+          tooltip: "Открыть карту",
+          icon: "bi bi-map",
+          onClicked: (action, data) =>
+            router.push({ name: routeNames.Map, params: { id: data.id } }),
+        },
+        {
+          tooltip: "Открыть отчёт",
+          icon: "bi bi-file-text",
+          onClicked: (action, data) =>
+            router.push({
+              /* name: reports */
+            }),
+          hide: (data) => !data.ready,
+        },
+      ] as Action<MapInfo>[],
+    },
+  },
 ];
 
 const options: GridOptions<MapInfo> = {
