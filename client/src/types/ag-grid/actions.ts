905b409c32d3f54aa51a36706a620ae560b7e1d2
Vlad <vladbolkunovv@gmail.com>
2023-03-11 14:35:17 +0300

    fix: action interface

--- client/src/types/ag-grid/actions.ts
+++ client/src/types/ag-grid/actions.ts
@@ -1,11 +1,6 @@
-import { ICellRendererParams } from "ag-grid-community";
-
 export interface Action<T = any> {
   tooltip: string;
   icon: string;
-  onClicked: <V = any>(
-    action: Action<T>,
-    params: ICellRendererParams<T, V>
-  ) => void;
-  hide?: boolean | (<V = any>(data: T, value: V) => boolean);
+  onClicked: (action: Action<T>, data: T) => void;
+  hide?: boolean | ((data: T) => boolean);
 }
