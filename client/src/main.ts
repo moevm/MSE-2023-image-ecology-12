7eb5ef1b6004e2868480723c4ec75e64ba65be8f
Vlad <vladbolkunovv@gmail.com>
2023-03-11 14:06:19 +0300

    fix: global tooltip use

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,5 +5,9 @@ import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { vBsTooltip } from "@/bootstrap/tooltip";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.directive("bs-tooltip", vBsTooltip);
+app.mount("#app");
