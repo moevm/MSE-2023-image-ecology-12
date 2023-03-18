22b528f019784058ca47f636fe2e9a3a0e1280b6
Vlad <vladbolkunovv@gmail.com>
2023-03-18 17:27:34 +0300

    feat: upload form

--- client/src/components/routes/upload/UploadComponent.vue
+++ client/src/components/routes/upload/UploadComponent.vue
@@ -1,7 +1,37 @@
 <template>
-  <div></div>
+  <div class="container mt-3">
+    <div class="m-auto col-10 col-lg-6">
+      <FormKit type="form" :actions="false" @submit="submit">
+        <FormKit
+          name="files"
+          type="file"
+          accept=".tiff,.jpg,.png"
+          label="Файл с картой"
+          validation="required"
+        />
+        <FormKit
+          name="name"
+          type="text"
+          label="Название карты"
+          validation="required"
+        />
+        <FormKit
+          outer-class="text-end"
+          input-class="$reset btn btn-success"
+          type="submit"
+          label="Загрузить"
+        />
+      </FormKit>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { uploadMap } from "@/components/routes/upload/api";
+
+function submit(data: { files: { name: string; file: File }[]; name: string }) {
+  uploadMap(data.files[0].file, data.name);
+}
+</script>
 
 <style scoped lang="scss"></style>
