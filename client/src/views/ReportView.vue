1c1b569f16ac20bcf6c4885b86e31ebcd871f80b
Vlad <vladbolkunovv@gmail.com>
2023-03-18 14:30:51 +0300

    feat: report component suspense

--- client/src/views/ReportView.vue
+++ client/src/views/ReportView.vue
@@ -1,9 +1,15 @@
 <template>
-  <ReportComponent :id="id" />
+  <Suspense>
+    <ReportComponent :id="id" />
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
 import ReportComponent from "@/components/routes/report/ReportComponent.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
 defineProps<{ id: number }>();
 </script>
 
