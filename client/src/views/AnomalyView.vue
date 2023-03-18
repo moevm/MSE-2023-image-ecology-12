3c1dcd1b63ba44b976d5c2a8a26b65c95c2428f8
Vlad <vladbolkunovv@gmail.com>
2023-03-18 15:07:48 +0300

    fix: anomaly suspense

--- client/src/views/AnomalyView.vue
+++ client/src/views/AnomalyView.vue
@@ -1,9 +1,15 @@
 <template>
-  <AnomalyComponent :id="id" />
+  <Suspense>
+    <AnomalyComponent :id="id" />
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
 import AnomalyComponent from "@/components/routes/anomaly/AnomalyComponent.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
 defineProps<{ id: number }>();
 </script>
 
