945142a801977ffeea10a6199c33b0dc537f1bfa
Vlad <vladbolkunovv@gmail.com>
2023-03-18 14:30:38 +0300

    feat: report data interface

--- client/src/types/reports.ts
+++ client/src/types/reports.ts
@@ -1,6 +1,13 @@
+import { MapAnomaly } from "@/types/maps";
+
 export interface ReportInfo {
   id: number;
   name: string;
   date: string;
   anomalies: number;
+}
+
+export interface ReportData {
+  mapId: number;
+  anomalies: MapAnomaly[];
 }
