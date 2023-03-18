ba08cd6c3ad9270614c2d1ffb390ed726c2e04ec
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-03-18 04:03:55 +0300

    feat: client update for display (WIP)

--- client/src/components/routes/map/api.ts
+++ client/src/components/routes/map/api.ts
@@ -1,39 +1,35 @@
-import { MapData } from "@/types/maps";
+import { Report } from "@/types/reports";
+import axios from "axios";
 
-export function getMapData(id: string): Promise<MapData | null> {
-  // Тестовые данные: чётный id - есть аномалии, нечётный -нет
-  return Promise.resolve(
-    (parseInt(id) % 2).toString()
-      ? null
-      : {
-          reportId: id,
-          anomalies: [
-            { id: "1", name: "Лес", coordinates: [10, 10], area: 100 },
-            {
-              id: "2",
-              name: "Полянка",
-              coordinates: [20, 15],
-              area: 200,
-            },
-            {
-              id: "4",
-              name: "Вырубка",
-              coordinates: [5, 5],
-              area: 300,
-            },
-            {
-              id: "3",
-              name: "Пожар",
-              coordinates: [5, 5],
-              area: 50,
-            },
-            {
-              id: "5",
-              name: "Пожар",
-              coordinates: [6, 6],
-              area: 50,
-            },
-          ],
-        }
-  );
+export async function getXMLinfo(id: string): Promise<Document> {
+  const serverURL = import.meta.env.SERVER_URI;
+  let xmlImageInfo: string = "";
+  await axios.get(serverURL + "/").then((response: string) => {
+    xmlImageInfo = response;
+  });
+  let parser: DOMParser = new DOMParser();
+  let xmlDoc: Document = parser.parseFromString(xmlImageInfo, "text/xml");
+  return xmlDoc;
+}
+
+export function getReport(id: number): Promise<Report> {
+  return Promise.resolve({
+    id,
+    groups: [
+      {
+        name: "Найденные объекты",
+        objects: [
+          { name: "Лес", coordinates: [10, 10] },
+          { name: "Полянка", coordinates: [20, 15] },
+        ],
+      },
+      {
+        name: "Найденные Аномалии",
+        objects: [
+          { name: "Вырубка", coordinates: [5, 5] },
+          { name: "Пожар", coordinates: [5, 5] },
+        ],
+      },
+    ],
+  });
 }
