7aa3ae51e02215758ae4ae967a09d1f1c61163e8
Vlad <vladbolkunovv@gmail.com>
2023-03-18 14:30:58 +0300

    feat: report data request

---
+++ client/src/components/routes/report/api.ts
@@ -0,0 +1,34 @@
+import { ReportData } from "@/types/reports";
+
+export function getReportData(id: number): Promise<ReportData> {
+  return Promise.resolve({
+    mapId: id,
+    anomalies: [
+      { id: 1, name: "Лес", coordinates: [10, 10], area: 100 },
+      {
+        id: 2,
+        name: "Полянка",
+        coordinates: [20, 15],
+        area: 200,
+      },
+      {
+        id: 4,
+        name: "Вырубка",
+        coordinates: [5, 5],
+        area: 300,
+      },
+      {
+        id: 3,
+        name: "Пожар",
+        coordinates: [5, 5],
+        area: 50,
+      },
+      {
+        id: 5,
+        name: "Пожар",
+        coordinates: [6, 6],
+        area: 50,
+      },
+    ],
+  });
+}
