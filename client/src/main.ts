613de87b9a84d3f42c841a57c6fa17a8ee4b54f9
Vlad <vladbolkunovv@gmail.com>
2023-03-18 17:27:20 +0300

    feat: formkit

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,8 +6,11 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { vBsTooltip } from "@/bootstrap/tooltip";
+import { plugin, defaultConfig } from "@formkit/vue";
+import "@formkit/themes/genesis";
 
 const app = createApp(App);
 app.use(router);
+app.use(plugin, defaultConfig());
 app.directive("bs-tooltip", vBsTooltip);
 app.mount("#app");
