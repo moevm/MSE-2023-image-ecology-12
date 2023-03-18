d76ce4b98a4d5aa68d6e7e567225ad2986381323
Vlad <vladbolkunovv@gmail.com>
2023-03-18 15:06:11 +0300

    fix: map link

--- client/src/components/routes/report/ReportComponent.vue
+++ client/src/components/routes/report/ReportComponent.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="container-lg">
     <h2 class="text-center mt-2 text-primary">Просмотр отчёта №{{ id }}</h2>
+    <div class="row justify-content-between">
+      <h3 class="col">Аномалии</h3>
+      <router-link
+        class="col-auto"
+        :to="{ name: routeNames.Map, params: { id: reportData.mapId } }"
+      >
+        <button class="btn btn-secondary">Открыть карту</button>
+      </router-link>
+    </div>
     <AgGridVue
       class="ag-theme-alpine mt-3"
       :row-data="reportData.anomalies"
       :column-defs="columnDefs"
       :grid-options="options"
+      @grid-ready="fitActionsColumn"
     />
   </div>
 </template>
@@ -15,7 +25,11 @@ import { AgGridVue } from "ag-grid-vue3";
 import { getReportData } from "@/components/routes/report/api";
 import { ColDef, GridOptions } from "ag-grid-community";
 import { AnomalyInfo } from "@/types/anomalies";
-import { getActionsColDef, getDefaultGridOptions } from "@/ag-grid/factory";
+import {
+  fitActionsColumn,
+  getActionsColDef,
+  getDefaultGridOptions,
+} from "@/ag-grid/factory";
 import { routeNames } from "@/router";
 import { useRouter } from "vue-router";
 
@@ -40,16 +54,6 @@ const columnDefs: ColDef<AnomalyInfo>[] = [
         tooltip: "Показать на карте",
         icon: "bi bi-eye",
         button: "btn-info",
-      },
-      {
-        tooltip: "Открыть карту",
-        icon: "bi bi-map",
-        button: "btn-primary",
-        onClicked: (action, data) =>
-          router.push({
-            name: routeNames.Map,
-            params: { id: reportData?.mapId },
-          }),
       },
     ]),
   },
