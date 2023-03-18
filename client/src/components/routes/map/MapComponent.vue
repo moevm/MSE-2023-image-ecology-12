1fe93724dddf483273535535269b49f3276a5fce
Vlad <vladbolkunovv@gmail.com>
2023-03-18 14:04:36 +0300

    fix: map component anomalies table

--- client/src/components/routes/map/MapComponent.vue
+++ client/src/components/routes/map/MapComponent.vue
@@ -1,21 +1,60 @@
 <template>
-  <h2 class="text-center mt-2 text-primary">Просмотр карты №{{ id }}</h2>
-  <div class="mt-4 row">
-    <div class="col-3">
-      <ObjectGroupsList :groups="report.groups" />
-    </div>
-    <div class="col">
+  <div class="container-lg">
+    <h2 class="text-center mt-2 text-primary">Просмотр карты №{{ id }}</h2>
+    <AgGridVue
+      class="ag-theme-alpine mt-3"
+      :row-data="mapData"
+      :column-defs="columnDefs"
+      :grid-options="options"
+      @grid-ready="fitActionsColumn"
+    />
+    <div class="d-flex justify-content-center mt-3">
       <img src="/src/assets/img.png" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getReport } from "@/components/routes/map/api";
-import ObjectGroupsList from "@/components/common/ObjectGroupsList.vue";
+import { getMapData } from "@/components/routes/map/api";
+import { AgGridVue } from "ag-grid-vue3";
+import { ColDef, GridOptions } from "ag-grid-community";
+import { AnomalyInfo } from "@/types/anomalies";
+import {
+  fitActionsColumn,
+  getActionsColDef,
+  getDefaultGridOptions,
+} from "@/ag-grid/factory";
+import { routeNames } from "@/router";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const columnDefs: ColDef<AnomalyInfo>[] = [
+  { headerName: "Id", field: "id", flex: 2 },
+  { headerName: "Название", field: "name", flex: 4 },
+  { headerName: "Площадь", field: "area", flex: 4 },
+  {
+    ...getActionsColDef([
+      {
+        tooltip: "Открыть аномалию",
+        icon: "bi bi-eye",
+        button: "btn-danger",
+        onClicked: (action, data) =>
+          router.push({ name: routeNames.Anomaly, params: { id: data.id } }),
+      },
+    ]),
+  },
+];
+
+const options: GridOptions<AnomalyInfo> = {
+  ...getDefaultGridOptions(),
+  pagination: true,
+  paginationPageSize: 4,
+  domLayout: "autoHeight",
+};
 
 const props = defineProps<{ id: number }>();
-const report = await getReport(props.id);
+const mapData = await getMapData(props.id);
 </script>
 
 <style scoped lang="scss"></style>
