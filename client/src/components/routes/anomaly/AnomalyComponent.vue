55f6319d5e83b2c9dc5d64765deb0d4903c31e2a
Vlad <vladbolkunovv@gmail.com>
2023-03-18 15:59:27 +0300

    feat: anomaly page

--- client/src/components/routes/anomaly/AnomalyComponent.vue
+++ client/src/components/routes/anomaly/AnomalyComponent.vue
@@ -1,9 +1,88 @@
 <template>
-  <div></div>
+  <div class="container-lg">
+    <h2 class="text-center mt-2 text-primary">Просмотр аномалии №{{ id }}</h2>
+    <div class="row justify-content-end">
+      <router-link
+        class="col-auto"
+        :to="{ name: routeNames.Map, params: { id: anomalyData?.mapId } }"
+      >
+        <button class="btn btn-secondary">Открыть карту</button>
+      </router-link>
+      <router-link
+        class="col-auto"
+        :to="{ name: routeNames.Report, params: { id: anomalyData?.reportId } }"
+      >
+        <button class="btn btn-primary">Открыть отчёт</button>
+      </router-link>
+    </div>
+
+    <AgGridVue
+      class="ag-theme-alpine mt-3"
+      :column-defs="columnDefs"
+      :grid-options="options"
+      :row-data="[anomalyData]"
+      style="height: 6rem"
+      @grid-ready="fitActionsColumn"
+    />
+
+    <div class="d-flex justify-content-center mt-3">
+      <img
+        v-bs-tooltip.right="'Тестовая демонстрация карты'"
+        src="/src/assets/img.png"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+import { ColDef, GridOptions } from "ag-grid-community";
+import { AnomalyInfo } from "@/types/anomalies";
+import { dateFormatter } from "@/ag-grid/formatters";
+import {
+  fitActionsColumn,
+  getActionsColDef,
+  getDefaultGridOptions,
+} from "@/ag-grid/factory";
+import { routeNames } from "@/router";
+import { getAnomalyData } from "@/components/routes/anomaly/api";
+import { AgGridVue } from "ag-grid-vue3";
+
+const router = useRouter();
+
 const props = defineProps<{ id: number }>();
+
+const columnDefs: ColDef<AnomalyInfo>[] = [
+  { headerName: "Название", field: "name", flex: 4 },
+  { headerName: "Площадь", field: "area", flex: 4 },
+  {
+    headerName: "Дата загрузки",
+    field: "uploadDate",
+    flex: 5,
+    valueFormatter: dateFormatter,
+  },
+  {
+    headerName: "Дата обнаружения",
+    field: "detectDate",
+    flex: 5,
+    valueFormatter: dateFormatter,
+  },
+  {
+    ...getActionsColDef([
+      {
+        tooltip: "Показать на карте",
+        icon: "bi bi-eye",
+        button: "btn-info",
+      },
+    ]),
+  },
+];
+
+const options: GridOptions<AnomalyInfo> = {
+  ...getDefaultGridOptions(),
+};
+
+const anomalyData = await getAnomalyData(props.id);
 </script>
 
 <style scoped lang="scss"></style>
