ab449607a9825a2002130ea4bc761b3315bbe27b
Vlad <vladbolkunovv@gmail.com>
2023-03-18 17:45:39 +0300

    fix: old request removed

--- client/src/components/routes/reports/api.ts
+++ client/src/components/routes/reports/api.ts
@@ -1,4 +1,4 @@
-import { Report, ReportInfo } from "@/types/reports";
+import { ReportInfo } from "@/types/reports";
 
 export function getReportsInfo(): Promise<ReportInfo[]> {
   return Promise.resolve([
@@ -13,28 +13,4 @@ export function getReportsInfo(): Promise<ReportInfo[]> {
     },
     { id: 5, date: "2023-03-01T12:01:10", anomalies: 5, name: "Лес 3" },
   ]);
-}
-
-export function getReportsList(): Promise<Report[]> {
-  return Promise.resolve(
-    [1, 2, 3, 4, 5, 6].map((i) => ({
-      id: i,
-      groups: [
-        {
-          name: "Найденные объекты",
-          objects: [
-            { name: "Лес", coordinates: [10, 10] },
-            { name: "Полянка", coordinates: [20, 15] },
-          ],
-        },
-        {
-          name: "Найденные Аномалии",
-          objects: [
-            { name: "Вырубка", coordinates: [5, 5] },
-            { name: "Пожар", coordinates: [5, 5] },
-          ],
-        },
-      ],
-    }))
-  );
 }
