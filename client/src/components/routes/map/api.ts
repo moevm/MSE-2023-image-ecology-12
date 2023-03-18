faf0e88c3d7c48178a9d4d73517e68bb47127cf8
Vlad <vladbolkunovv@gmail.com>
2023-03-18 14:04:14 +0300

    fix: map anomalies request

--- client/src/components/routes/map/api.ts
+++ client/src/components/routes/map/api.ts
@@ -1,23 +1,11 @@
-import { Report } from "@/types/reports";
+import { MapAnomaly } from "@/types/maps";
 
-export function getReport(id: number): Promise<Report> {
-  return Promise.resolve({
-    id,
-    groups: [
-      {
-        name: "Найденные объекты",
-        objects: [
-          { name: "Лес", coordinates: [10, 10] },
-          { name: "Полянка", coordinates: [20, 15] },
-        ],
-      },
-      {
-        name: "Найденные Аномалии",
-        objects: [
-          { name: "Вырубка", coordinates: [5, 5] },
-          { name: "Пожар", coordinates: [5, 5] },
-        ],
-      },
-    ],
-  });
+export function getMapData(id: number): Promise<MapAnomaly[]> {
+  return Promise.resolve([
+    { id: 1, name: "Лес", coordinates: [10, 10], area: 100 },
+    { id: 2, name: "Полянка", coordinates: [20, 15], area: 200 },
+    { id: 4, name: "Вырубка", coordinates: [5, 5], area: 300 },
+    { id: 3, name: "Пожар", coordinates: [5, 5], area: 50 },
+    { id: 5, name: "Пожар", coordinates: [6, 6], area: 50 },
+  ]);
 }
