1cf419157ac96290b27cee81dc7d8e97df215657
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-03-20 04:14:23 +0300

    feat: client update for display

--- client/src/components/routes/map/api.ts
+++ client/src/components/routes/map/api.ts
@@ -1,12 +1,12 @@
 import { MapData } from "@/types/maps";
 import { Report } from "@/types/reports";
 import axios from "axios";
+import { baseURL } from "@/api";
 
 export async function getXMLinfo(id: string): Promise<Document> {
-  const serverURL = import.meta.env.SERVER_URI;
   let xmlImageInfo: string = "";
-  await axios.get(serverURL + "/").then((response: string) => {
-    xmlImageInfo = response;
+  await axios.get<string>(baseURL + "/images/tile_map_resource/" + id).then((response: any) => {
+    xmlImageInfo = response.data;
   });
   let parser: DOMParser = new DOMParser();
   let xmlDoc: Document = parser.parseFromString(xmlImageInfo, "text/xml");
