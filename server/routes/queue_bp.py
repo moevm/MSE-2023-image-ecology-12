2573d0755714197d5003fcb270c30bf34f9fa98b
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-03-25 12:47:35 +0300

    feat: update dependencies

--- server/routes/queue_bp.py
+++ server/routes/queue_bp.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, g, current_app, Response, send_file
+from db import get_db, get_grid_fs
+from werkzeug.local import LocalProxy
+from bson.objectid import ObjectId
+import io
 
 queue_bp = Blueprint('queue', __name__, url_prefix='/queue')
 
 
-@queue_bp.route('/', methods=['POST'])
+@queue_bp.route('/<image_id>', methods=['POST'])
 def add_to_queue():
     """
         This route would be used to add images to the analysis queue for batch processing.
@@ -11,6 +15,10 @@ def add_to_queue():
     # Add an image to the analysis queue
     return jsonify({'status': 'success'})
 
+@images_bp.route('/<image_id>', methods=['GET'])
+def get_image(image_id):
+    image_file = fs.get(ObjectId(image_id))
+    return send_file(image_file, mimetype='image/tiff')
 
 @queue_bp.route('/', methods=['GET'])
 def get_queue():
