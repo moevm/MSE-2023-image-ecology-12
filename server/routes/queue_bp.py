818bf3525b8455a3f207a6e8ec069f1dd7c3fbf3
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-03-25 13:45:41 +0300

    fix: fixed id type

--- server/routes/queue_bp.py
+++ server/routes/queue_bp.py
@@ -3,28 +3,32 @@ from db import get_db, get_grid_fs
 from werkzeug.local import LocalProxy
 from bson.objectid import ObjectId
 import io
+from pymongo import updateOne
 
 queue_bp = Blueprint('queue', __name__, url_prefix='/queue')
+db = LocalProxy(get_db)
+fs = LocalProxy(get_grid_fs)
+images_bp = Blueprint('images_bp', __name__)
 
-
-@queue_bp.route('/<image_id>', methods=['POST'])
+@queue_bp.route('/<string:db_id>', methods=['POST'])
 def add_to_queue():
     """
         This route would be used to add images to the analysis queue for batch processing.
     """
-    # Add an image to the analysis queue
+
     return jsonify({'status': 'success'})
 
-@images_bp.route('/<image_id>', methods=['GET'])
+@images_bp.route('/<string:db_id>', methods=['GET'])
 def get_image(image_id):
     image_file = fs.get(ObjectId(image_id))
     return send_file(image_file, mimetype='image/tiff')
 
-@queue_bp.route('/', methods=['GET'])
+@queue_bp.route('/<string:db_id>', methods=['GET'])
 def get_queue():
     """
             This route would return the current state of the image analysis queue,
             including the number of images waiting to be processed and their estimated processing time.
     """
     # Return the current state of the analysis queue
+
     return jsonify({'queue': []})
