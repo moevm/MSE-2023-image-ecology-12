1ff7d4f9a76838f39d02c78116b491100c9c2424
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-03-28 14:25:59 +0300

    fix: updated geotiff2png

--- worker/image_processing/geotiff_slicer/geotiff2png.py
+++ worker/image_processing/geotiff_slicer/geotiff2png.py
@@ -4,7 +4,7 @@ from osgeo import gdal
 def geotiffToPng(
         geotiffBytes,
         pngName, 
-        optionsList = ['if GTiff', '-ot Byte', '-of PNG', '-b 1', '-scale']
+        optionsList = ['if GTiff', '-ot Byte', '-of PNG', '-b 1', '-b 2', '-b 3', '-scale']
     ):
     """
     Function to convert geotiff file to png.
