5cdfeead53966eb01a65c9e4d21260b7bc6e4dcf
SergevninDmitry <71733518+SergevninDmitry@users.noreply.github.com>
2023-04-01 13:54:06 +0300

    images_bp search_bp update

--- server/routes/search_bp.py
+++ server/routes/search_bp.py
@@ -1,12 +1,36 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
+from db import get_db
+from werkzeug.local import LocalProxy
+from bson import json_util
+from datetime import datetime, timedelta
+import json
 
+
+db = LocalProxy(get_db)
 search_bp = Blueprint('search', __name__, url_prefix='/search')
 
 
-@search_bp.route('/', methods=['GET'])
-def search_images():
+@search_bp.route('/test')
+def index():
+    return """
+        <form method="POST" action="/api/search/search_images" enctype="multipart/form-data">
+            <input type='text' name="date">
+            <input type="submit">
+        </form> 
+    """
+
+@search_bp.route('/search_images/<string:date>', methods=['GET'])
+def search_images(date):
     """
         Allows users to search for specific images by date, location, and other metadata.
     """
+    date = datetime.fromisoformat(date)
+    documents = db['images'].find({'uploadDate': {'$gte': date, '$lt': date + timedelta(days=1)}})
+    documents = [{'_id': str(document['_id']),
+                  'filename': document.get('filename', None),
+                  'tile_map_resource': document.get('tile_map_resource', None),
+                  'fs_id': str(document['fs_id']),
+                  'uploadDate': document.get('uploadDate', None)} for document in documents]
     # Search for specific images
-    return jsonify({'results': []})
+    print(documents)
+    return jsonify(documents)
