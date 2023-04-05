97762dd44c01d2072e0f3300d9415a81f7d31044
Vlad <vladbolkunovv@gmail.com>
2023-04-05 11:27:11 +0300

    refactor: lint

--- client/src/components/common/map/MapDisplay.vue
+++ client/src/components/common/map/MapDisplay.vue
@@ -1,10 +1,10 @@
 <template>
-    <div id="map"></div>
+  <div id="map"></div>
 </template>
-  
+
 <script setup lang="ts">
 import { getXMLinfo, getForestPolygon } from "@/components/common/map/api";
-import { onMounted } from 'vue'
+import { onMounted } from "vue";
 import { baseURL } from "@/api";
 
 import "leaflet/dist/leaflet.css";
@@ -15,60 +15,92 @@ const xmlImageInfoDoc: Document = await getXMLinfo(props.id);
 let forestPolygonArr: number[][][] = await getForestPolygon(props.id);
 
 onMounted(() => {
-    //  OpenStreetMap.
-    let osm: L.Layer = L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"});
+  //  OpenStreetMap.
+  let osm: L.Layer = L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution:
+      "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
+  });
 
-    // Overlay layers (TMS).
-    let lyr: L.Layer = L.tileLayer(baseURL + "/images/tile/" + props.id + "/{z}/{x}/{y}", {tms: true, opacity: 1, attribution: ""});
+  // Overlay layers (TMS).
+  let lyr: L.Layer = L.tileLayer(
+    baseURL + "/images/tile/" + props.id + "/{z}/{x}/{y}",
+    { tms: true, opacity: 1, attribution: "" }
+  );
 
-    // Forest Polygon Layer.
-    let forestPolygon: Polygon = L.polygon(forestPolygonArr as LatLngExpression[][], 
-        {color: 'green', fillOpacity: 0.4}
-    );
-    let forestPolygonLayer: L.LayerGroup = L.layerGroup([forestPolygon]);
+  // Forest Polygon Layer.
+  let forestPolygon: Polygon = L.polygon(
+    forestPolygonArr as LatLngExpression[][],
+    { color: "green", fillOpacity: 0.4 }
+  );
+  let forestPolygonLayer: L.LayerGroup = L.layerGroup([forestPolygon]);
 
-    // Map.
-    let map: L.Map = L.map('map', {
-        center: [
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("Origin")[0].attributes[0].nodeValue as string),  // x
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("Origin")[0].attributes[1].nodeValue as string)  // y
-        ],
-        zoom: parseInt(xmlImageInfoDoc.getElementsByTagName("TileSet")[0].attributes[0].nodeValue as string),
-        minZoom: parseInt(xmlImageInfoDoc.getElementsByTagName("TileSet")[0].attributes[0].nodeValue as string),
-        maxZoom: parseInt(xmlImageInfoDoc.getElementsByTagName("TileSet")[xmlImageInfoDoc.getElementsByTagName("TileSet").length - 1].attributes[0].nodeValue as string),
-        layers: [osm]
-    });
+  // Map.
+  let map: L.Map = L.map("map", {
+    center: [
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("Origin")[0].attributes[0]
+          .nodeValue as string
+      ), // x
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("Origin")[0].attributes[1]
+          .nodeValue as string
+      ), // y
+    ],
+    zoom: parseInt(
+      xmlImageInfoDoc.getElementsByTagName("TileSet")[0].attributes[0]
+        .nodeValue as string
+    ),
+    minZoom: parseInt(
+      xmlImageInfoDoc.getElementsByTagName("TileSet")[0].attributes[0]
+        .nodeValue as string
+    ),
+    maxZoom: parseInt(
+      xmlImageInfoDoc.getElementsByTagName("TileSet")[
+        xmlImageInfoDoc.getElementsByTagName("TileSet").length - 1
+      ].attributes[0].nodeValue as string
+    ),
+    layers: [osm],
+  });
 
-    let basemaps = {"OpenStreetMap": osm}
-    let overlaymaps = {
-        "Image": lyr,
-        "<span style='color: green'>Forest </span>":  forestPolygonLayer
-    }
+  let basemaps = { OpenStreetMap: osm };
+  let overlaymaps = {
+    Image: lyr,
+    "<span style='color: green'>Forest </span>": forestPolygonLayer,
+  };
 
+  // Add base layers
+  L.control.layers(basemaps, overlaymaps, { collapsed: false }).addTo(map);
 
-    // Add base layers
-    L.control.layers(basemaps, overlaymaps, {collapsed: false}).addTo(map);
-
-    // Fit to overlay bounds (SW and NE points with (lat, lon))
-    map.fitBounds([
-        [
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[1].nodeValue as string), // miny
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[2].nodeValue as string)  // maxx
-        ], 
-        [
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[3].nodeValue as string),  // maxy
-            parseFloat(xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[0].nodeValue as string)  // minx
-        ] 
-    ]);
-})
-
+  // Fit to overlay bounds (SW and NE points with (lat, lon))
+  map.fitBounds([
+    [
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[1]
+          .nodeValue as string
+      ), // miny
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[2]
+          .nodeValue as string
+      ), // maxx
+    ],
+    [
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[3]
+          .nodeValue as string
+      ), // maxy
+      parseFloat(
+        xmlImageInfoDoc.getElementsByTagName("BoundingBox")[0].attributes[0]
+          .nodeValue as string
+      ), // minx
+    ],
+  ]);
+});
 </script>
-  
+
 <style scoped lang="scss">
 #map {
-    width: 90%;
-    height: 600px;
-    overflow: hidden;
+  width: 90%;
+  height: 600px;
+  overflow: hidden;
 }
 </style>
-  
