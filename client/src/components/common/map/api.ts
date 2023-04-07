6fe79bb0b9786c11c4c38afec6163615fd1dc2f9
Vlad <vladbolkunovv@gmail.com>
2023-04-07 20:28:29 +0300

    refactor: lint

--- client/src/components/common/map/api.ts
+++ client/src/components/common/map/api.ts
@@ -2,13 +2,17 @@ import axios from "axios";
 import { baseURL } from "@/api";
 
 export async function getXMLinfo(id: string): Promise<Document> {
-  let xmlImageInfo: string = (await axios.get<string>(baseURL + "/images/tile_map_resource/" + id)).data;
-  let parser: DOMParser = new DOMParser();
-  let xmlDoc: Document = parser.parseFromString(xmlImageInfo, "text/xml");
+  const xmlImageInfo: string = (
+    await axios.get<string>(baseURL + "/images/tile_map_resource/" + id)
+  ).data;
+  const parser: DOMParser = new DOMParser();
+  const xmlDoc: Document = parser.parseFromString(xmlImageInfo, "text/xml");
   return xmlDoc;
 }
 
 export async function getForestPolygon(id: string): Promise<number[][][]> {
-  let forestPolygon: number[][][] = (await axios.get<number[][][]>(baseURL + "/images/forest/" + id)).data;
+  const forestPolygon: number[][][] = (
+    await axios.get<number[][][]>(baseURL + "/images/forest/" + id)
+  ).data;
   return forestPolygon;
 }
