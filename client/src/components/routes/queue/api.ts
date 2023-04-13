7e4a38494fa061fc59fab252f985c9c4d44e04a8
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-04-13 17:43:03 +0300

    fix: changed the type of REST requests

--- client/src/components/routes/queue/api.ts
+++ client/src/components/routes/queue/api.ts
@@ -1,75 +1,23 @@
 import { QueueItemInfo } from "@/types/queue";
-import { QueueStatus } from "@/config/queue";
-import axios from "axios";
 import { api } from "@/api";
 
 export function higherQueueApi(id: string){
-  const formData = new FormData();
-  formData.append("db_id", id);
-  return api.post("/queue/higher", formData);
+  return api.put("/queue/higher/" + id);
 }
 
 export function lowerQueueApi(id: string){
-  const formData = new FormData();
-  formData.append("db_id", id);
-  return api.post("/queue/lower", formData);
+  return api.put("/queue/lower/" + id);
 }
 
 export function upQueueApi(id: string){
-  const formData = new FormData();
-  formData.append("db_id", id);
-  return api.post("/queue/up", formData);
+  return api.put("/queue/up/" + id);
 }
 
 export function downQueueApi(id: string){
-  const formData = new FormData();
-  formData.append("db_id", id);
-  return api.post("/queue/down", formData);
+  return api.put("/queue/down/" + id);
 }
 
-export function getQueueInfo(): Promise<QueueItemInfo[]> {
-  return  api.get<QueueItemInfo[]>("/queue/get_queue").then(function (res) {
-            return res.data;
-        })
+export async function getQueueInfo(): Promise<QueueItemInfo[]> {
+  return (await api.get<QueueItemInfo[]>("/queue/get_queue")).data
 }
-
-/*export function getQueueInfo(): Promise<QueueItemInfo[]> {
-  return Promise.resolve([
-    {
-      id: "1",
-      uploadDate: "2023-02-20T12:10:09",
-      progress: 90,
-      status: QueueStatus.processing,
-      name: "Лес 5",
-    },
-    {
-      id: "2",
-      uploadDate: "2023-02-20T13:20:04",
-      progress: 0,
-      status: QueueStatus.enqueued,
-      name: "Лес 6",
-    },
-    {
-      id: "3",
-      uploadDate: "2023-02-20T14:43:29",
-      progress: 30,
-      status: QueueStatus.stopped,
-      name: "Лес 7",
-    },
-    {
-      id: "4",
-      uploadDate: "2023-02-20T15:36:39",
-      progress: 50,
-      status: QueueStatus.enqueued,
-      name: "Лес 8",
-    },
-    {
-      id: "5",
-      uploadDate: "2023-02-20T12:53:02",
-      progress: 0,
-      status: QueueStatus.enqueued,
-      name: "Лес 9",
-    },
-  ]);
-}*/
 
