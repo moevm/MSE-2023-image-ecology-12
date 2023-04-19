59b74694b19d4033fd9a3e9156c7e7914203593f
Vlad <vladbolkunovv@gmail.com>
2023-04-20 01:16:27 +0300

    feat: slice & process tasks using

--- server/app/routes/images_bp.py
+++ server/app/routes/images_bp.py
@@ -1,13 +1,17 @@
-import requests
 from flask import Blueprint, jsonify, request, send_file
-from db import get_db, get_grid_fs,get_worker_url
+from app.db import get_db, get_tile_fs, get_map_fs
+
 from werkzeug.local import LocalProxy
 from bson.objectid import ObjectId
 import io
 
+from app.tasks.slice import slice
+from app.tasks.image_process import thresholding_otsu
+
 db = LocalProxy(get_db)
-fs = LocalProxy(get_grid_fs)
-worker_url = LocalProxy(get_worker_url)
+tileFs = LocalProxy(get_tile_fs)
+mapFs = LocalProxy(get_map_fs)
+
 images_bp = Blueprint('images_bp', __name__, url_prefix="/images")
 
 
@@ -30,11 +34,8 @@ def index(db_id):
 
 @images_bp.route('/upload_image', methods=['POST'])
 def add_image():
-    """
-        Adds a new image to the system for analysis.
-    """
     image = request.files['image']
-    file_id = fs.put(image, filename=image.filename, chunk_size=256 * 1024)
+    file_id = mapFs.put(image, filename=image.filename, chunk_size=256 * 1024)
     item = {
         "filename": image.filename,
         "tile_map_resource": None,
@@ -44,8 +45,8 @@ def add_image():
     }
 
     db.images.insert_one(item)
-    worker_res = requests.put(worker_url + "slice/" + str(file_id))
-    worker_res = requests.put(worker_url + "thresholding_otsu/" + str(file_id))
+    slice.delay(str(file_id))
+    thresholding_otsu.delay(str(file_id))
     return jsonify({'message': 'Image added successfully'})
 
 
@@ -53,33 +54,28 @@ def add_image():
 @images_bp.route("/tile/<string:db_id>/<int:z>/<int:x>/<int:y>", methods=['GET'])
 def get_tile(db_id, z, x, y):
     image_name = db.images.find_one(ObjectId(db_id))["filename"]
-    tile_info = fs.find_one({"filename": f"{image_name[:image_name.rfind('.')]}_{z}_{x}_{y}.png"})
+    tile_info = tileFs.find_one({"filename": f"{image_name[:image_name.rfind('.')]}_{z}_{x}_{y}.png"})
     if (tile_info):
-        tile = fs.get(tile_info._id).read()
+        tile = tileFs.get(tile_info._id).read()
         return send_file(io.BytesIO(tile), mimetype='image/png')
     else:
         return 'OK'
 
+
 @images_bp.route('/<string:db_id>', methods=['GET'])
 def get_image(db_id):
     image_info = db.images.find_one(ObjectId(db_id))
-    image_file = fs.get(image_info["fs_id"])
+    image_file = tileFs.get(image_info["fs_id"])
     return send_file(io.BytesIO(image_file), mimetype='image/tiff')
 
 
 @images_bp.route('/forest/<string:db_id>', methods=['GET'])
 def get_image_forest(db_id):
-    """
-        Returns polygon of find forest in image.
-    """
     image_info = db.images.find_one(ObjectId(db_id))
     return image_info["forest_polygon"]
 
 
 @images_bp.route('/<string:db_id>/analysis', methods=['GET'])
 def get_image_analysis(db_id):
-    """
-        Returns the results of the analysis for a specific image, including the detected deviations and their locations.
-    """
     # analysis = ImageService.get_image_analysis(image_id)
     return jsonify({'analysis': []})  # jsonify(analysis)
