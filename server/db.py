d1cfd7516a004ec7411fdca05617aa4a24bab4aa
Vlad <vladbolkunovv@gmail.com>
2023-04-19 22:29:09 +0300

    refactor: tile & map filesystems

--- server/db.py
+++ server/db.py
@@ -1,44 +1,37 @@
-import os
 import pymongo
+import redis
 from flask import g
 from gridfs import GridFS
-
-
-def init_db():
-    print("Init db")
-    return get_db()
+from app import app
 
 
 def get_db():
-    """
-    Configuration method to return db instance
-    """
-    db = getattr(g, "_database", None)
-
+    db = getattr(g, "database", None)
     if db is None:
-        DB_URI = os.environ['DB_URI'] if ('DB_URI' in os.environ) else "mongodb://localhost:27017/db"
-        client = pymongo.MongoClient(DB_URI)  # config['PROD']['DB_URI']
+        client = pymongo.MongoClient(app.config.get('MONGO_URI'))  # config['PROD']['DB_URI']
         # Если есть база данных ecologyDB
-        db = g._database = client.get_database('ecologyDB')
-
+        db = g.database = client.get_database('ecologyDB')
     return db
 
 
+@app.teardown_appcontext
 def close_db(error=None):
-    db = g.pop('db', None)
-
+    db = g.pop('database', None)
     if db is not None:
         db.close()
 
 
-def get_grid_fs():
-    db = get_db()
-
-    fs = getattr(g, "_fs", None)
-
+def get_map_fs():
+    fs = getattr(g, "map_fs", None)
     if fs is None:
-        fs = g._fs = GridFS(db)
+        fs = g.map_fs = GridFS(get_db())
+    return fs
 
+
+def get_tile_fs():
+    fs = getattr(g, "tile_fs", None)
+    if fs is None:
+        fs = g.tile_fs = GridFS(get_db())
     return fs
 
 
