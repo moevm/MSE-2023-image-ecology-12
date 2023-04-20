a0cb93ee9436b88fc38ef2dbf9431465601555a3
Vlad <vladbolkunovv@gmail.com>
2023-04-20 23:18:53 +0300

    feat: production flask run

--- server/run.py
+++ server/run.py
@@ -1,3 +1,10 @@
+import waitress
+
 from app import app
 port = app.config.get('FLASK_PORT')
-app.run(host='0.0.0.0', port=port)
+
+if app.config.get('DEBUG'):
+    app.run(host='0.0.0.0', port=port)
+
+else:
+    waitress.serve(app, host='0.0.0.0', port=port)
