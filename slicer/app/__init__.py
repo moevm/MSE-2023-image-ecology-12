97a7eb8132ac6752de730efce04f81041028cb38
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-04-23 14:29:22 +0300

    fix: here we go again (slicer functionality to worker)

--- slicer/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from celery import Celery
-from app import config
-
-app = Celery(include=['app.tasks.slice'])
-app.conf.broker_url = config.REDIS_URI
-app.conf.result_backend = config.REDIS_URI
--- slicer/app/config.py
+++
@@ -1,4 +0,0 @@
-from os import environ
-
-MONGO_URI = environ.get('MONGO_URI', "mongodb://localhost:27017/db")
-REDIS_URI = environ.get('REDIS_URI', "redis://localhost:6379/0")
