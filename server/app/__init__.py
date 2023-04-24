53c31da9cf09ddd2d272f214dc3d1796d0c04aeb
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-04-24 14:22:33 +0300

    fix: moved dev test part in server

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,7 +6,3 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.register_blueprint(api_bp, url_prefix="/api")
 CORS(app)
-
-# Раскоментируй эту строчку, если хочешь очистить базу данных при запуске сервера (тестовый режим).
-# delete_all_data_in_db_and_fs(application, db)
-# add_test_data_db(application, db, fs, worker_url)
--- server/run.py
+++ server/run.py
@@ -1,10 +1,13 @@
 import waitress
 from app import app
+from app.tasks import delete_all_data_in_db_and_fs
+
+# Раскоментируй эту строчку, если хочешь очистить базу данных при запуске сервера (тестовый режим).
+# delete_all_data_in_db_and_fs.delay()
 
 port = app.config.get('FLASK_PORT')
 
 if app.config.get('DEBUG'):
     app.run(host='0.0.0.0', port=port)
-
 else:
     waitress.serve(app, host='0.0.0.0', port=port)
