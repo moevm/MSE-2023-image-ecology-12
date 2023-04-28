63115b1f76666855f438c2f5f64afd7f508424cd
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-04-29 00:12:02 +0300

    fix: removed unnecessary functions, added vrt file entry to memory, corrected file names in memory.

--- worker/app/image_processing/geotiff_slicer/slice2tiles.py
+++ worker/app/image_processing/geotiff_slicer/slice2tiles.py
@@ -19,45 +19,11 @@ def sliceToTiles(
     - optionsTranslate - list of options for gdal_translate (Translate options to convert 16 bit images to 8 bit).
     - optionsSliceToTiles - dict of options for slicing (for gdal2tiles).
     """
-    gdal.FileFromMemBuffer("/vsimem/img.tiff", geotiffBytes)
-    image = gdal.Open("/vsimem/img.tiff")
+    gdal.FileFromMemBuffer(f"/vsimem/{geotiffName}.tiff", geotiffBytes)
+    image = gdal.Open(f"/vsimem/{geotiffName}.tiff")
 
-    _preprocessGeotiff(geotiffName, image, optionsTranslate)
-    _slicePreprocessedGeotiff(geotiffName[:geotiffName.rfind('.')] + '.vrt', slicesOutputPath, optionsSliceToTiles)
+    gdal.Translate(f'/vsimem/{geotiffName}.vrt', image, options=" ".join(optionsTranslate))
+    gdal2tiles.generate_tiles(f'/vsimem/{geotiffName}.vrt', slicesOutputPath, **optionsSliceToTiles)
 
-    os.remove(geotiffName[:geotiffName.rfind('.')] + '.vrt')
-    gdal.Unlink("/vsimem/img.tiff")
-
-
-def _preprocessGeotiff(
-        geotiffName,
-        geotiffDataset,
-        optionsTranslate
-):
-    """
-    Function which converts geotiff files into a shape with the possibility of successful slicing (and save res to .vrt).
-    - geotiffName - name of geotiff file for preparing.
-    - geotiffDataset - gdal dataset of geotiff file for preparing.
-    - optionsTranslate - options for gdal.Translate (Translate options to convert 16 bit images to 8 bit).
-    """
-    # Translate and create new vrt file
-    gdal.Translate(geotiffName[:geotiffName.rfind('.')] + '.vrt',
-                   geotiffDataset,
-                   options=" ".join(optionsTranslate)
-                   )
-
-
-def _slicePreprocessedGeotiff(
-        geotiffPath,
-        slicesOutputPath,
-        optionsSliceToTiles
-):
-    """
-    Function that cuts a geotiff file into fragments that are available for display in leaflet.js.
-    - geotiffPath - path to geotiff file for slicing.
-    - slicesOutputPath - path to the directory where the received slices will be stored.
-    - optionsSliceToTiles - options for slicing (for gdal2tiles). 
-    """
-    if not os.path.exists(slicesOutputPath):
-        os.makedirs(slicesOutputPath)
-    gdal2tiles.generate_tiles(geotiffPath, slicesOutputPath, **optionsSliceToTiles)
+    gdal.Unlink(f'/vsimem/{geotiffName}.vrt')
+    gdal.Unlink(f"/vsimem/{geotiffName}.tiff")
