0fde801351ba8fe033a0e01318a897a7f0f8f58b
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-05-26 18:41:50 +0300

    feat: added test support for coordinates in meters

--- worker/app/image_processing/coordinates_transform/transform_coordinates.py
+++ worker/app/image_processing/coordinates_transform/transform_coordinates.py
@@ -13,7 +13,11 @@ class CoordintesTransformer:
         self.image_file = rasterio.MemoryFile(geotif_bytes)
         self.image = self.image_file.open()
         self.lat_long_crs = CRS.from_epsg(4326)
+        self.meters_crs = CRS.from_epsg(32724)
         self.image_crs = self.image.crs
+
+        self.width = self.image.width
+        self.height = self.image.height
 
     def pixel_xy_to_lat_long(self, x, y):
         xy_in_image_crs = self.image.xy(y, x)
@@ -21,6 +25,21 @@ class CoordintesTransformer:
             long_lat = rasterio.warp.transform(
                 self.image_crs,
                 self.lat_long_crs,
+                [xy_in_image_crs[0]],
+                [xy_in_image_crs[1]]
+            )
+            # return [lattitude, longitude]
+            return long_lat[1][0], long_lat[0][0]
+        else:
+            # return [lattitude, longitude]
+            return xy_in_image_crs[1], xy_in_image_crs[0]
+        
+    def pixel_xy_to_meters(self, x, y):
+        xy_in_image_crs = self.image.xy(y, x)
+        if (self.image_crs != self.lat_long_crs):
+            long_lat = rasterio.warp.transform(
+                self.image_crs,
+                self.meters_crs,
                 [xy_in_image_crs[0]],
                 [xy_in_image_crs[1]]
             )
