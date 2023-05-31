c6d5bb819482deeefdd8f17a5b0f69d11181dbb5
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-06-01 02:27:10 +0300

    feat: added route for one image anomalies

--- server/app/routes/anomalies_bp.py
+++ server/app/routes/anomalies_bp.py
@@ -16,7 +16,7 @@ anomalies_bp = Blueprint('anomalies_bp', __name__, url_prefix="/anomalies")
 
 
 @anomalies_bp.route('/', methods=['GET'])
-def get_anomalies_list():
+def get_anomalies_info_list():
     anomalies = []
     for img in db.images.find({}):
         img_anomalies_types = img["anomalies"]
@@ -31,6 +31,34 @@ def get_anomalies_list():
                         "uploadDate": img["upload_date"],
                         "detectDate": img["detect_date"]
                     })
+    return anomalies
+
+
+@anomalies_bp.route('/<string:img_id>', methods=['GET'])
+def get_anomalies_of_image(img_id):
+    img = db.images.find_one(ObjectId(img_id))
+    img_anomalies_types = img["anomalies"]
+
+    anomalies = []
+    for img_anomalies in img_anomalies_types:
+        if (img_anomalies['name'] != 'Forest'):
+            for i in range(len(img_anomalies['area'])):
+                coordinates = [0, 0]
+                for polygon_point in img_anomalies['polygons'][int(i)]:
+                    coordinates[0] += polygon_point[0]
+                    coordinates[1] += polygon_point[1]
+                coordinates[0] /= len(img_anomalies['polygons'][int(i)])
+                coordinates[1] /= len(img_anomalies['polygons'][int(i)])
+
+                anomalies.append({
+                    "id": str(img_id),
+                    "name": img_anomalies["name"],
+                    "anomalyIndex": i,
+                    "area": img_anomalies["area"][i],
+                    "coordinates": coordinates,
+                    "uploadDate": img["upload_date"],
+                    "detectDate": img["detect_date"] 
+                })
     return anomalies
 
 
@@ -52,8 +80,7 @@ def get_anomaly(img_id, anomaly_name, anomaly_index):
             break
     
     result = {
-        "reportId": "0", #TODO
-        "mapId": str(img_id),
+        "id": str(img_id),
         "name": anomaly_name,
         "anomalyIndex": anomaly_index,
         "area": area,
