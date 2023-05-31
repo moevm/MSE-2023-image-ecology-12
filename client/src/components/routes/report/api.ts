6709a38a29af602b5a87672e7052e4ec2685c7a3
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-05-31 22:51:15 +0300

    feat: Added pipline

--- client/src/components/routes/report/api.ts
+++ client/src/components/routes/report/api.ts
@@ -1,34 +1,7 @@
 import { ReportData } from "@/types/reports";
+import axios from "axios";
+import { baseURL } from "@/api";
 
-export function getReportData(id: string): Promise<ReportData> {
-  return Promise.resolve({
-    mapId: id,
-    anomalies: [
-      { id: "1", name: "Лес", coordinates: [10, 10], area: 100 },
-      {
-        id: "2",
-        name: "Полянка",
-        coordinates: [20, 15],
-        area: 200,
-      },
-      {
-        id: "4",
-        name: "Вырубка",
-        coordinates: [5, 5],
-        area: 300,
-      },
-      {
-        id: "3",
-        name: "Пожар",
-        coordinates: [5, 5],
-        area: 50,
-      },
-      {
-        id: "5",
-        name: "Пожар",
-        coordinates: [6, 6],
-        area: 50,
-      },
-    ],
-  });
+export async function getReportData(id: string): Promise<ReportData> {
+  return (await axios.get<ReportData>(baseURL + "/reports/" + id)).data;
 }
