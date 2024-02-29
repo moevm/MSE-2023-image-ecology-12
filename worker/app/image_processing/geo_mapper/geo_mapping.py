fb0661be856e83fb155629eaf091e4a93e73f609
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-02-29 19:41:24 +0300

    fix

--- worker/app/image_processing/geo_mapper/geo_mapping.py
+++ worker/app/image_processing/geo_mapper/geo_mapping.py
@@ -12,8 +12,4 @@ def geo_mapping(input_geotiff_file: str, pixel_x: int, pixel_y: int) -> LatLon:
     with rasterio.open(input_geotiff_file) as src:
         geo_transform = src.transform
         geo_coordinates = geo_transform * (pixel_x, pixel_y)
-        
-        print(f"Географические координаты для пикселя ({pixel_x}, {pixel_y}):")
-        print(f"Долгота (Longitude): {geo_coordinates[0]}")
-        print(f"Широта (Latitude): {geo_coordinates[1]}")
-    return LatLon(lon=geo_transform[0], lan=geo_coordinates[1])
+    return LatLon(lon=geo_coordinates[0], lan=geo_coordinates[1])
