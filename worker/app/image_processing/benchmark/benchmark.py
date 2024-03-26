201232068a7e0f9517833130d9c84260473343cd
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-26 13:54:07 +0300

    multitime measure | exclude time to measure

--- worker/app/image_processing/benchmark/benchmark.py
+++ worker/app/image_processing/benchmark/benchmark.py
@@ -1,10 +1,12 @@
 import time
 import glob
-import rasterio
 import sys
 import os
+import warnings
+from rasterio.errors import NodataShadowWarning
+warnings.filterwarnings('ignore', category=NodataShadowWarning)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from image_processing.compress_image.compress import compress
 from image_processing.crop_image.crop import crop_image
 
@@ -14,30 +16,50 @@ def read_geotiff(file_path):
         return f.read()
 
 
-def benchmark(files, operation, *args, **kwargs):
+def avg(arr: list) -> float:
+    if len(arr) == 0:
+        return float('+inf')
+    return sum(arr) / len(arr)
+
+
+def time_to_measure_time():
+    s = time.time_ns()
+    e = time.time_ns()
+    return e - s
+
+
+def benchmark(files, operation, *args, n_runs=10, **kwargs):
     times = []
 
     for file_path in files:
-        input_bytes = read_geotiff(file_path)
+        file_times = []
 
-        start_time = time.time_ns()
+        for _ in range(n_runs):
+            input_bytes = read_geotiff(file_path)
 
-        output_bytes = operation(input_bytes, *args, **kwargs)
+            start_time = time.time_ns()
+            output_bytes = operation(input_bytes, *args, **kwargs)
+            end_time = time.time_ns()
 
-        end_time = time.time_ns()
+            elapsed_time = end_time - start_time
+            file_times.append(elapsed_time)
 
-        elapsed_time = end_time - start_time
-        times.append(elapsed_time)
+            print(
+                f"{operation.__name__} выполнение {len(file_times)} для {file_path} заняло {elapsed_time:.4f} наносекунд"
+            )
 
-        print(f"{operation.__name__} для {file_path} заняло {elapsed_time:.4f} наносекунд")
-
+        time_delations = [time_to_measure_time() for _ in range(n_runs)]
+        avg_time_delation = avg(time_delations)
+        avg_file_time = avg(file_times)
+        times.append(avg_file_time - avg_time_delation)
+        print(f"Среднее время для {file_path}: {avg_file_time:.4f} наносекунд\n")
     return sum(times) / len(times)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     geotiff_files = glob.glob("../map_samples/*.tif")
-
-    avg_crop_time = benchmark(geotiff_files, crop_image, 0, 0, 1000, 1000)
+    print(geotiff_files)
+    avg_crop_time = benchmark(geotiff_files, crop_image, 0, 0, 1000, 1000, n_runs=10)
     print(f"Среднее время обрезки: {avg_crop_time:.4f} наносекунд")
 
     avg_compress_time = benchmark(geotiff_files, compress)
