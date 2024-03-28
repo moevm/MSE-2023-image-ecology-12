f0819a69a57804deedff69f59c79863858cbfe2d
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-28 13:12:04 +0300

    f

--- worker/app/image_processing/compress_image/compress.py
+++ worker/app/image_processing/compress_image/compress.py
@@ -1,17 +1,17 @@
 from rasterio.io import MemoryFile
 from rasterio.enums import Compression
 
+
 def compress(input_bytes: bytes) -> bytes:
     with MemoryFile(input_bytes) as memfile:
         with memfile.open() as src:
             profile = src.profile
             profile.update(compression=Compression.lzw)
-            
+
             with MemoryFile() as mem_dst:
                 with mem_dst.open(**profile) as dst:
                     for i in range(1, src.count + 1):
                         data = src.read(i)
                         dst.write(data, i)
-                
-                # Получаем сжатые данные обратно в байтах
+
                 return mem_dst.read()
